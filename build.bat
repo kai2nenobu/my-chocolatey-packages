1f0c21fb9910e385a1736f9c0fb27fa7a22440d9
Tsunenobu Kai <kai2nenobu@gmail.com>
2017-10-17 16:58:59 +0900

    Additionally build a5m2 package in a for statement

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 
+for %%p in (a5m2 cpdf-cr) do (
+echo Build a package for %%p
+
 REM Build package
-cd %SourcesPath%\cpdf-cr
+cd %SourcesPath%\%%p
 choco pack
 
 REM Verify package
 call test.bat
+)
