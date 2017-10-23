73f92c80eca75ca97319622e52673bea6104a393
Tsunenobu Kai <kai2nenobu@gmail.com>
2017-10-24 08:04:08 +0900

    Exit abnormally if error will happen

--- nkf/build.bat
+++ nkf/build.bat
@@ -12,16 +12,22 @@ set NKF_URL=https://osdn.net/projects/nkf/downloads/64158/%NKF_DIR%.tar.gz/
 set ARCHIVE=%NKF_DIR%.tar.gz
 
 "%CURL%" -sSfL -o "%ARCHIVE%" "%NKF_URL%"
-"%GZ%" --decompress --stdout "%ARCHIVE%" | "%TAR%" x
-cd "%NKF_DIR%"
+if errorlevel 1 exit /b %ERRORLEVEL%
 
+"%GZ%" --decompress --stdout "%ARCHIVE%" | "%TAR%" x
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+cd "%NKF_DIR%"
 REM Make nkf
-"%MAKE%" CC="%GCC%" CFLAGS="-g -O2 -Wall -pedantic -DDEFAULT_NEWLINE=0x0D0A -DDEFAULT_CODE_WINDOWS_31J"
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 REM Copy to bin direcory
 cd ..
 mkdir bin
 copy "%NKF_DIR%\nkf.exe" bin
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+REM Print nkf version
 bin\nkf -V
 
 endlocal
