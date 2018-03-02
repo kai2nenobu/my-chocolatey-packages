eb888888edb49b82a3809f38ea62ab4d360a5970
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-03-03 07:15:46 +0900

    Remove a shortcut for GUI during uninstall

---
+++ mape/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+## Include common configurations
+$scriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. "$scriptDirectory\common.ps1"
+
+## Remove a shortcut for GUI program
+Remove-Item $guiShortcut
