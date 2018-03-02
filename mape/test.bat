cf7f48066c1da5b6cbee649f27367d19448fa5aa
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-03-03 07:24:41 +0900

    Add a test script

---
+++ mape/test.bat
@@ -0,0 +1,30 @@
+setlocal
+
+set PACKAGE_NAME=mape
+set %CLI_NAME%=mape
+set %GUI_NAME%=mapegui
+
+REM Install a package in local
+choco install -y -s . -pre %PACKAGE_NAME%
+
+REM Verify command is in PATH
+where %CLI_NAME%
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+where %GUI_NAME%
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+REM Uninstall a package
+choco uninstall -y %PACKAGE_NAME%
+
+REM Verify command is removed successfully
+where %CLI_NAME%
+if "%ERRORLEVEL%"=="1" (
+    echo Package is successfully removed
+    exit /b 0
+) else (
+    echo Try to uninstall a package but %CLI_NAME% command is remaining in PATH
+    exit /b 1
+)
+
+endlocal
