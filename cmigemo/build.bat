fab90b9f4e1800f5b580cd39b01f685fe853f9b6
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-06-16 01:40:05 +0900

    Build cmigemo (v1.4-rc) from source (#10)

---
+++ cmigemo/build.bat
@@ -0,0 +1,29 @@
+setlocal enabledelayedexpansion
+
+REM Constants
+set MSYS2_SHELL=%MSYS2_ROOT%\msys2_shell.cmd
+set GIT_COMMAND=C:\Program Files\Git\cmd\git.exe
+
+set CMIGEMO_VERSION=v1.4-rc
+set CMIGEMO_REPO=https://github.com/koron/cmigemo.git
+
+REM Install nkf for encoding conversion
+choco source add -n kai2nenobu -s https://www.myget.org/F/kai2nenobu
+choco install -y nkf
+
+REM Fetch source
+"%GIT_COMMAND%" clone --depth 1 --branch "%CMIGEMO_VERSION%" "%CMIGEMO_REPO%" src
+cd src
+git rev-parse HEAD
+
+REM Build by mingw32
+"%MSYS2_SHELL" -mingw32 -here -c "(sh configure --prefix=../cmigemo;  mingw32-make ICONV_EUCJP_TO_CP932='nkf -x --ic=eucjp-ms --oc=shift_jis' ICONV_CP932_TO_UTF8='nkf -x --ic=shift_jis --oc=utf-8' mingw-install;) > build.log 2>&1"
+type build.log
+
+REM Print version
+..\cmigemo\bin\cmigemo --help
+
+REM DEBUG
+dir /S ..\
+
+endlocal
