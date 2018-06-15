98fcc47f1e7e8a791716c58135741b3ecc9118d3
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-06-16 03:20:19 +0900

    Save cmigemo dir

--- cmigemo/build.bat
+++ cmigemo/build.bat
@@ -24,8 +24,7 @@ cd src
 "%GIT_COMMAND%" rev-parse HEAD
 
 REM Build by mingw32
-"%MSYS2_SHELL%" -mingw32 -here -c "sh configure --prefix=../cmigemo; mingw32-make ICONV_EUCJP_TO_CP932='nkf -x --ic=eucjp-ms --oc=shift_jis' ICONV_CP932_TO_UTF8='nkf -x --ic=shift_jis --oc=utf-8' mingw-install > build.log 2>&1"
-dir .
+"%MSYS2_SHELL%" -mingw32 -no-start -here -c "sh configure --prefix=../cmigemo; mingw32-make ICONV_EUCJP_TO_CP932='nkf -x --ic=eucjp-ms --oc=shift_jis' ICONV_CP932_TO_UTF8='nkf -x --ic=shift_jis --oc=utf-8' mingw-install > build.log 2>&1"
 type build.log
 
 REM Print version
