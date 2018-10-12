cbeb5dd001569dae6ede2e85829eb317526ea052
Tsunenobu Kai <kai2nenobu@gmail.com>
2018-10-12 15:50:16 +0900

    Bump a version to 2018.09.20181004

--- pleiades/build.ps1
+++ pleiades/build.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop';  # stop on all errors
 [string[]]$editions = 'standard', 'full'
 
 $Pleiades = @{
-  Version='2018.09.20181004-pre'
+  Version='2018.09.20181004'
   platform=@{
     Title='Pleiades All in One Platform'
     Tag=''
