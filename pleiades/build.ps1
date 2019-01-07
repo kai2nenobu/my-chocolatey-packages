aa5d39768e88d45c1afbb04916015d5089082773
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-01-07 09:17:45 +0900

    Remove 32bit package information
    Because 32bit Eclipse 2018-12 is not provided.

--- pleiades/build.ps1
+++ pleiades/build.ps1
@@ -12,14 +12,10 @@ $Pleiades = @{
     standard=@{
       Url='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-platform-win-32bit_20181004.zip'
       Checksum='1c23d9a9e67b944ff526b67db704858c'
-      Url64='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-platform-win-64bit_20181004.zip'
-      Checksum64='d8f293ccc383e2b71457701e8a5afefe'
     }
     full=@{
       Url='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-platform-win-32bit-jre_20181004.zip'
       Checksum='4d1ce8adc329dcfff645ea5ad0eb8461'
-      Url64='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-platform-win-64bit-jre_20181004.zip'
-      Checksum64='3f1e699d2da93db59d0309accf58a8f3'
     }
   }
   java=@{
@@ -28,14 +24,10 @@ $Pleiades = @{
     standard=@{
       Url='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-java-win-32bit_20181004.zip'
       Checksum='0be5a93235e2a192bdaa3705a1d2e001'
-      Url64='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-java-win-64bit_20181004.zip'
-      Checksum64='dbe7d3ad89a809c07d91d91c52239bac'
     }
     full=@{
       Url='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-java-win-32bit-jre_20181004.zip'
       Checksum='ad8774957171ed38580467b8c563b0f5'
-      Url64='http://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2018/pleiades-2018-09-java-win-64bit-jre_20181004.zip'
-      Checksum64='7f63a0cedf87d083f3c4249cf7700fdd'
     }
   }
 }
@@ -75,8 +67,6 @@ foreach ($lang in $languages) {
       $_  -replace '{{PackageTitle}}', $Pleiades[$lang].Title `
         -replace '{{Url}}', $Pleiades[$lang][$ed].Url `
         -replace '{{Checksum}}', $Pleiades[$lang][$ed].Checksum `
-        -replace '{{Url64}}', $Pleiades[$lang][$ed].Url64 `
-        -replace '{{Checksum64}}', $Pleiades[$lang][$ed].Checksum64
     } | Out-File $targetScript -Encoding UTF8
     ## Packaging
     choco pack $targetNuspec
--- pleiades/template/ChocolateyInstall.tmpl.ps1
+++ pleiades/template/ChocolateyInstall.tmpl.ps1
@@ -4,8 +4,6 @@ $packageName = $env:ChocolateyPackageName
 $installPath = Join-Path (Get-ToolsLocation) $packageName
 $url         = '{{Url}}'
 $checksum    = '{{Checksum}}'
-$url64       = '{{Url64}}'
-$checksum64  = '{{Checksum64}}'
 $checksumType = 'md5'
 
 ## Download and install pleaides in a tools directory (Typically "C:\tools")
@@ -14,9 +12,6 @@ Install-ChocolateyZipPackage `
   -Url $url `
   -Checksum $checksum `
   -ChecksumType $checkstumType `
-  -Url64bit $url64 `
-  -Checksum64 $checksum64 `
-  -ChecksumType64 $checkstumType `
   -UnzipLocation $installPath
 
 ## Create a shortcut to eclipse.exe
