3774bbcccf7cfbdfd58e7d28d9bcd3c676539dcb
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-04-01 00:44:38 +0900

    Fix a patch command path

--- mozc-emacs-helper/build.bat
+++ mozc-emacs-helper/build.bat
@@ -1,6 +1,7 @@
 setlocal enabledelayedexpansion
 
 set GIT_COMMAND=C:\Program Files\Git\cmd\git
+set PATCH_COMMAND=%MSYS2_ROOT%\usr\bin\patch
 
 set MOZC_REPOSITORY=https://github.com/google/mozc.git
 set COMMIT_HASH=afb03ddfe72dde4cf2409863a3bfea160f7a66d8
@@ -15,7 +16,7 @@ cd mozc
 "%GIT_COMMAND%" submodule update --init --recursive
 
 REM Build mozc
-patch -p1 < ..\win32_build.patch
+"%PATCH_COMMAND%" -p1 < ..\win32_build.patch
 cd src
 python build_mozc.py gyp --noqt --branding=GoogleJapaneseInput
 python build_mozc.py build -c Release package
@@ -23,6 +24,8 @@ if errorlevel 1 exit /b %ERRORLEVEL%
 
 REM Copy executable to bin direcory
 cd ..\..\
+dir /S mozc\src\out_win\Release
+dir /S mozc\src\out_win\Release_x64
 mkdir Release
 mkdir Release_x64
 copy mozc\src\out_win\Release\mozc_emacs_helper.exe Release
