8f083f65fd171ec59d7a4b212d901d9bf65b5a1e
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-06-07 17:57:06 +0900

    Add an update script using AU
    Implement global:au_GetLatest function which returns "Version",
    "URL64" and "URL32".
    Implemente global:au_SearchReplace function which replace variables in
    scripts.
    
    Any other tasks like comparing versions and caluculating checksums are
    au's jobs.

---
+++ aws-sam-cli/update.ps1
@@ -0,0 +1,33 @@
+Import-Module au
+
+$releases = 'https://api.github.com/repos/awslabs/aws-sam-cli/releases'
+
+function global:au_GetLatest {
+  ## Find a latest release and extract installer URL from GitHub Releases
+  $releases_info = Invoke-RestMethod -Uri $releases
+  foreach ($release in $releases_info) {
+    if (-not $release.prerelease) {
+      $version = $release.tag_name -replace "^v",""
+      $url64 = $release.assets | Where-Object { $_.name -like "AWS_SAM_CLI_64*.msi" } | Select-Object -First 1 -Expand browser_download_url
+      $url32 = $release.assets | Where-Object { $_.name -like "AWS_SAM_CLI_32*.msi" } | Select-Object -First 1 -Expand browser_download_url
+      return @{
+        Version = $version
+        URL64 = $url64
+        URL32 = $url32
+      }
+    }
+  }
+}
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+          "(^\s*Url)\s*=.*" = "`${1} = '$($Latest.URL32)'"
+          "(^\s*Url64bit)\s*=.*" = "`${1} = '$($Latest.URL64)'"
+          "(^\s*Checksum)\s*=.*" = "`${1} = '$($Latest.Checksum32)'"
+          "(^\s*Checksum64)\s*=.*" = "`${1} = '$($Latest.Checksum64)'"
+        }
+    }
+}
+
+Update-Package -NoReadme
