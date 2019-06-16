9f791a9ebda96d8aafb510197693e3b0cc8c622b
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-06-16 09:02:40 +0900

    Use globbing to get a full path of zip

--- ghq/tools/chocolateyInstall.ps1
+++ ghq/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $is64      = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
 
 $packageArgs = @{
     PackageName    = 'ghq'
-    FileFullPath   = "$toolsPath\ghq_windows_386.zip"
-    FileFullPath64 = "$toolsPath\ghq_windows_amd64.zip"
+    FileFullPath   = Get-Item "$toolsPath\*_windows_386.zip"
+    FileFullPath64 = Get-Item "$toolsPath\*_windows_amd64.zip"
     Destination    = $toolsPath
 }
 
