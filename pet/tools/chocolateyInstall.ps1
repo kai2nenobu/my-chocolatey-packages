1fae844d9b38b177a9beb70ba5dbc21919faa687
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-06-16 14:17:42 +0900

    Expand twice .tar.gz archive

--- pet/tools/chocolateyInstall.ps1
+++ pet/tools/chocolateyInstall.ps1
@@ -2,13 +2,21 @@
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
+ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+
+# Expand .tar.gz
 $packageArgs = @{
     PackageName    = 'pet'
     FileFullPath   = Get-Item "$toolsPath\*_windows_386.tar.gz"
     FileFullPath64 = Get-Item "$toolsPath\*_windows_amd64.tar.gz"
     Destination    = $toolsPath
 }
-
-ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
 Get-ChocolateyUnzip @packageArgs
+
+# Expand .tar
+$packageArgs.FileFullPath = $packageArgs.FileFullPath -replace '\.gz$',''
+$packageArgs.FileFullPath64 = $packageArgs.FileFullPath64 -replace '\.gz$',''
+Get-ChocolateyUnzip @packageArgs
+
 rm $toolsPath\*.tar.gz -ea 0
+rm $toolsPath\*.tar -ea 0
