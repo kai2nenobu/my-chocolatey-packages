1b8b2a3de69efc0173cbe650cbc5ab2a22c4351f
Chocolatey <chocolatey@realdimensions.net>
2019-08-01 00:06:04 +0000

    AU: 1 updated - aws-sam-cli [skip ci] https://gist.github.com/4df33f42a891ced2fe169974fd3d58ec/de34741e6e0a22ed222ad33f867390f87965684f

--- aws-sam-cli/tools/chocolateyInstall.ps1
+++ aws-sam-cli/tools/chocolateyInstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   PackageName = $packageName
   FileType = 'msi'
   SilentArgs = '/quiet'
-  Url = 'https://github.com/awslabs/aws-sam-cli/releases/download/v0.18.0/AWS_SAM_CLI_32_PY3.msi'
-  Url64bit = 'https://github.com/awslabs/aws-sam-cli/releases/download/v0.18.0/AWS_SAM_CLI_64_PY3.msi'
+  Url = 'https://github.com/awslabs/aws-sam-cli/releases/download/v0.19.0/AWS_SAM_CLI_32_PY3.msi'
+  Url64bit = 'https://github.com/awslabs/aws-sam-cli/releases/download/v0.19.0/AWS_SAM_CLI_64_PY3.msi'
   ChecksumType = 'sha256'
-  Checksum = '5790c9f4c42dc27626acb98e41fca29cb641f3bd7a5c9d19e72f19b24c4579b7'
-  Checksum64 = '2c645a978f6be4cba235ce7fe710b3ac5e1e41afafe81a4425b35c08976c65d5'
+  Checksum = 'e2557cd9c0cfe1c24b46ed4ae2a04c945c2f7fec18b5517f207a21c593942ba5'
+  Checksum64 = 'df716bd6b0748a56458f4828acb39ade6fc351e7a573bb801e5a25e5784cafa9'
 }
 
 
