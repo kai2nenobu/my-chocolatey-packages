6e4aa7459532375f099d1ae7ee966461190c4a47
Chocolatey <chocolatey@realdimensions.net>
2019-08-28 16:06:14 +0000

    AU: 1 updated - a5m2 [skip ci] https://gist.github.com/4df33f42a891ced2fe169974fd3d58ec/026ecca805425a1cf058807e3246eef372358177

--- a5m2/tools/chocolateyInstall.ps1
+++ a5m2/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = $env:ChocolateyPackageName
 $unzipLocation = Join-Path $env:ChocolateyPackageFolder 'a5m2'
-$url = 'https://a5m2.mmatsubara.com/download/a5m2_2.14.2_x86.zip'
-$checksum = '4f496befaea79d5dc4c31c7296849be9623c89147b3b3c82665435ab20b1b5c3'
-$url64 = 'https://a5m2.mmatsubara.com/download/a5m2_2.14.2_x64.zip'
-$checksum64 = 'ba58661f798615948f7f145f287f60000979073478c881822fe053cd5c108c30'
+$url = 'https://a5m2.mmatsubara.com/download/a5m2_2.14.3_x86.zip'
+$checksum = '7a18759a451a032e3fedf2d06f300f7e31b24175d86300f7cfbb8417b71b89bd'
+$url64 = 'https://a5m2.mmatsubara.com/download/a5m2_2.14.3_x64.zip'
+$checksum64 = '52c25393725d906538f5313c6c2cee9b17e8dd578b4810fa011eff8a0ae4701e'
 
 # if ($env:ChocolateyForceX86 -Or !$env:OS_IS64BIT) {
 #   $subDirectory = 'cmigemo-default-win32'
