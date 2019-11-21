c7c7c561e0cc0d7a816653c7afecb94a563bbd26
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-11-21 18:26:28 +0900

    Fix a process name

--- wsl-ssh-agent/tools/chocolateyBeforeModify.ps1
+++ wsl-ssh-agent/tools/chocolateyBeforeModify.ps1
@@ -1,3 +1,3 @@
-Write-Host "Kill all 'wsl-ssh-agent' processes..."
-## TODO: Gracefully stop wsl-ssh-agent process
+Write-Host "Kill all 'wsl-ssh-agent-gui' processes..."
+## TODO: Gracefully stop wsl-ssh-agent-gui process
 Stop-Process -Name "[w]sl-ssh-agent-gui" -Force
