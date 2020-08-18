f83c776cc95a82db9ac527c0e771cfe233907241
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-18 23:14:30 +0900

    Add script to install and uninstall HackGen

---
+++ hackgen/tools/ChocolateyBeforeModify.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+## Include common configurations
+$toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. (Join-Path $toolsDir 'common.ps1')
+
+$hackgenFonts | ForEach-Object {
+  'Uninstalling {0}...' -f $_
+  Uninstall-ChocolateyFont $_ | Out-Null
+}
---
+++ hackgen/tools/ChocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+
+## Include common configurations
+$toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+. (Join-Path $toolsDir 'common.ps1')
+
+$hackgenBase = 'HackGen_v2.0.0'
+$archivePath = (Join-Path $toolsDir "${hackgenBase}.zip")
+$extractDir = (Join-Path $toolsDir $hackgenBase)
+
+
+$packageArgs = @{
+  PackageName    = 'hackgen'
+  FileFullPath   = $archivePath
+  Destination    = $toolsDir
+}
+
+# Extract Zip
+Get-ChocolateyUnzip @packageArgs
+
+# Install all ttf fonts
+$hackgenFonts | ForEach-Object {
+ 'Installing {0}...' -f $_
+  Install-ChocolateyFont (Join-Path $extractDir $_) | Out-Null
+}
