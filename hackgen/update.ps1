c4c0780a2392aae3ccd4499439aa2c9c5ab90ee2
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-19 13:23:06 +0900

    Fix typo

--- hackgen/update.ps1
+++ hackgen/update.ps1
@@ -117,7 +117,7 @@ function global:au_GetLatest {
   ## Find a latest release and extract zip URL from GitHub Releases
   #$releases = 'https://api.github.com/repos/yuru7/HackGen/releases'
   #$releases_info = Invoke-RestMethod -Uri $releases
-  $releases_info = ($fixedVesion | ConvertFrom-Json)
+  $releases_info = ($fixedVersion | ConvertFrom-Json)
   foreach ($release in $releases_info) {
     $tag = $release.tag_name
     if ($release.prerelease) {
