56d88a0fb511689edfb335d997b0debb15df37fe
Tsunenobu Kai <kai2nenobu@gmail.com>
2020-08-19 09:45:50 +0900

    Download a zip on installation instead of embedding it in the package
    Embedding a zip enlarge a size of the package. I want to avoid it.

--- hackgen/tools/ChocolateyInstall.ps1
+++ hackgen/tools/ChocolateyInstall.ps1
@@ -4,22 +4,23 @@
 $toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . (Join-Path $toolsDir 'common.ps1')
 
-$hackgenBase = 'HackGen_v2.0.0'
-$archivePath = (Join-Path $toolsDir "${hackgenBase}.zip")
+$hackgenVersion = 'v2.0.0'
+$hackgenBase = "HackGen_${hackgenVersion}"
 $extractDir = (Join-Path $toolsDir $hackgenBase)
 
-
 $packageArgs = @{
-  PackageName    = 'hackgen'
-  FileFullPath   = $archivePath
-  Destination    = $toolsDir
+  PackageName   = 'hackgen'
+  Url           = "https://github.com/yuru7/HackGen/releases/download/${hackgenVersion}/${hackgenBase}.zip"
+  Checksum      = '5ad5c40f2476cd37bf48a496da3ebc5f08c2d817ea4d672fe39c6cb93d91010a'
+  ChecksumType  = 'sha256'
+  UnzipLocation = $toolsDir
 }
 
-# Extract Zip
-Get-ChocolateyUnzip @packageArgs
+# Download and extract Zip
+Install-ChocolateyZipPackage @packageArgs
 
 # Install all ttf fonts
 $hackgenFonts | ForEach-Object {
- 'Installing {0}...' -f $_
+  'Installing {0}...' -f $_
   Install-ChocolateyFont (Join-Path $extractDir $_) | Out-Null
 }
