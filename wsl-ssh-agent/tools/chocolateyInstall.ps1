64c980ca248ba31a564ed947e8e885020989a1d8
Tsunenobu Kai <kai2nenobu@gmail.com>
2021-03-10 21:07:26 +0900

    Change the archive name to download

--- wsl-ssh-agent/tools/chocolateyInstall.ps1
+++ wsl-ssh-agent/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = 'wsl-ssh-agent'
-  FileFullPath64 = "$toolsPath\wsl-ssh-agent.7z"
+  FileFullPath64 = "$toolsPath\wsl-gpg-agent.zip"
   Destination    = $toolsPath
 }
 
 Get-ChocolateyUnzip @packageArgs
-rm $toolsPath\*.7z -ea 0
+rm $toolsPath\*.zip -ea 0
