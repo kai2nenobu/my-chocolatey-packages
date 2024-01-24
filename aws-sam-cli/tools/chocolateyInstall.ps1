ed2d4242f5f6ff75764a70c932fb3397b050d1f7
Chocolatey <chocolatey@realdimensions.net>
2024-01-24 00:26:49 +0000

    AU: 1 updated - aws-sam-cli [skip ci] https://gist.github.com/kai2nenobu/4df33f42a891ced2fe169974fd3d58ec/18ec3f25bdc2ab4355ac1c07ef458dffc4e5457f

--- aws-sam-cli/tools/chocolateyInstall.ps1
+++ aws-sam-cli/tools/chocolateyInstall.ps1
@@ -7,9 +7,9 @@ $packageArgs = @{
   PackageName = $packageName
   FileType = 'msi'
   SilentArgs = '/quiet'
-  Url64bit = 'https://github.com/aws/aws-sam-cli/releases/download/v1.107.0/AWS_SAM_CLI_64_PY3.msi'
+  Url64bit = 'https://github.com/aws/aws-sam-cli/releases/download/v1.108.0/AWS_SAM_CLI_64_PY3.msi'
   ChecksumType = 'sha256'
-  Checksum64 = 'f6a2f15187b64e62aad8ecc7b2a78560fce88fae34ce1bfc4ea96d619ae39a71'
+  Checksum64 = '84b7b53cbe28a3f5758a5373046260a4bf2359d23a1cd37b83b1608036d79317'
 }
 
 
