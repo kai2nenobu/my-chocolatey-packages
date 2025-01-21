54fac8ae03f957cfb050d707ccb8cd78ead2d3cf
Chocolatey <chocolatey@realdimensions.net>
2025-01-21 00:42:13 +0000

    AU: 1 updated - pleiades-platform [skip ci] https://gist.github.com/kai2nenobu/4df33f42a891ced2fe169974fd3d58ec/9fc2c519bb368911a74124e6816ed98d13f5430e

--- pleiades-streams/tools/chocolateyInstall.ps1
+++ pleiades-streams/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'pleiades-platform'
 $installPath = Join-Path (Get-ToolsLocation) $packageName
-$url = 'https://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2024/pleiades-2024-12-platform-win-64bit_20241228.exe'
-$checksum = '2f22a101abb3b6b6cc85889d204fe5dd'
+$url = 'https://ftp.jaist.ac.jp/pub/mergedoc/pleiades/2024/pleiades-2024-12-platform-win-64bit_20250120.exe'
+$checksum = '1217e69e2f77c64a3329b914fb81ca78'
 $checksumType = 'md5'
 $shortcutName = 'Pleiades All in One Platform Standard Edition.lnk'
 
